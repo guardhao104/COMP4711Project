38c82376dece6b8932d8e505591c56dc94457eb2
Hao Guan <guardhao@outlook.com>
2018-11-14 16:16:44 -0800

    first API getScore is down

--- functions/index.js
+++ functions/index.js
@@ -1,62 +1,25 @@
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
-
 // The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
 const functions = require('firebase-functions');
 
 // The Firebase Admin SDK to access the Firebase Realtime Database.
 const admin = require('firebase-admin');
 
-// var serviceAccount = require("path/to/serviceAccountKey.json");
+// Fetch the service account key JSON file contents
+var serviceAccount = require("./a1-f09ef-firebase-adminsdk-fzg9l-da2606e857.json");
 
-// // Initialize the app with a service account, granting admin privileges
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount),
-//   databaseURL: "https://databaseName.firebaseio.com"
-// });
+// Initialize the app with a service account, granting admin privileges
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://a1-f09ef.firebaseio.com"
+  });
 
-// // As an admin, the app has access to read and write all data, regardless of Security Rules
-// var db = admin.database();
-
-admin.initializeApp();
-
-// // Take the text parameter passed to this HTTP endpoint and insert it into the
-// // Realtime Database under the path /messages/:pushId/original
-// exports.addMessage = functions.https.onRequest((req, res) => {
-//   // Grab the text parameter.
-//   const original = req.query.text;
-//   // Push the new message into the Realtime Database using the Firebase Admin SDK.
-//   return admin.database().ref('/messages').push({original: original}).then((snapshot) => {
-//     // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
-//     return res.redirect(303, snapshot.ref.toString());
-//   });
-// });
-
-// // Listens for new messages added to /messages/:pushId/original and creates an
-// // uppercase version of the message to /messages/:pushId/uppercase
-// exports.makeUppercase = functions.database.ref('/messages/{pushId}/original')
-//     .onCreate((snapshot, context) => {
-//       // Grab the current value of what was written to the Realtime Database.
-//       const original = snapshot.val();
-//       console.log('Uppercasing', context.params.pushId, original);
-//       const uppercase = original.toUpperCase();
-//       // You must return a Promise when performing asynchronous tasks inside a Functions such as
-//       // writing to the Firebase Realtime Database.
-//       // Setting an "uppercase" sibling in the Realtime Database returns a Promise.
-//       return snapshot.ref.parent.child('uppercase').set(uppercase);
-//     });
-
+// Usage: https://us-central1-a1-f09ef.cloudfunctions.net/getScore?id=USER_ID&ver=QUIZ_VERSION
 exports.getScore = functions.https.onRequest((req, res) => {
     const userId = req.query.id;
-    return admin.database().ref('/users' + userId).once('value').then(function(snapshot) {
+    const quizVersion = req.query.ver;
+    return admin.database().ref('/users/' + userId + '--' + quizVersion + '--').once('value').then(function(snapshot) {
         var email = (snapshot.val() && snapshot.val().email) || 'Anonymous';
         var score = (snapshot.val() && snapshot.val().score) || 'No record';
-        console.log(email);
-        console.log(score);
         return res.json({
             email: email,
             score: score
