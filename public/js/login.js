067c3b7b115d5b22e2ea0eed7ee27a6a528c0466
Joselito Ayeras <jose_ayeras@yahoo.com>
2018-11-16 10:46:25 -0800

    made better error handling but not completely finished

--- public/js/login.js
+++ public/js/login.js
@@ -4,16 +4,28 @@ function toggleSignIn() {
       firebase.auth().signOut();
       // [END signout]
     } else {
+      var safe = true;
       var email = document.getElementById('email').value;
       var password = document.getElementById('password').value;
+      var e_wrap = document.getElementById("email-wrapper");
+      var p_wrap = document.getElementById("password-wrapper");
+      message.className = "";
+      message.innerHTML = "";
       if (email.length < 4) {
         noEmailErrMsg();
-        return;
+        safe = false;
+      }
+      else {
+        e_wrap.className = "form-group";
       }
       if (password.length < 4) {
         noPasswordErrMsg();
-        return;
+        safe = false;
       }
+      else {
+        p_wrap.className = "form-group";
+      }
+      if(!safe) return;
       // Sign in with email and pass.
       // [START authwithemail]
       firebase.auth().signInWithEmailAndPassword(email, password).then( user =>{
@@ -48,16 +60,28 @@ function toggleSignIn() {
    * Handles the sign up button press.
    */
   function handleSignUp() {
+    var safe = true;
     var email = document.getElementById('email').value;
     var password = document.getElementById('password').value;
+    var e_wrap = document.getElementById("email-wrapper");
+    var p_wrap = document.getElementById("password-wrapper");
+    message.className = "";
+    message.innerHTML = "";
     if (email.length < 4) {
       noEmailErrMsg();
-      return;
+      safe = false;
+    }
+    else {
+      e_wrap.className = "form-group";
     }
     if (password.length < 4) {
       noPasswordErrMsg();
-      return;
+      safe = false;
     }
+    else {
+      p_wrap.className = "form-group";
+    }
+    if(!safe) return;
     // Sign in with email and pass.
     // [START createwithemail]
     firebase.auth().createUserWithEmailAndPassword(email, password).then( () => {
@@ -118,16 +142,25 @@ function toggleSignIn() {
   window.onload = function() {
     initApp();
   };
+function clearErr(){
+  document.getElementById("email-wrapper").className = "form-group";
+  document.getElementById("password-wrapper").className = "form-group";
+  message.className = "";
+  message.innerHTML = "";
+}
 function noEmailErrMsg(){
-  message.innerHTML = "Please enter an email address.";
+  message.innerHTML += "Please enter an email address.<br/>";
+  message.className = "error-feedback";
   //message.className += " form-group has-warning has-feedback";
   //document.getElementById("message-glyph").className = "glyphicon glyphicon-warning form-control-feedback";
   document.getElementById("email-wrapper").className = "form-group has-error has-feedback";
+  //document.getElementById("email").className = "mdl-textfield__input form-group has-error has-feedback";
   //document.getElementById("email-glyph").className = "glyphicon glyphicon-remove form-control-feedback";
 };
 function noPasswordErrMsg(){
-  document.getElementById("password-wrapper").className = "form-group has-warning has-feedback";
-  message.innerHTML = "Please enter a password.";
+  message.innerHTML += "Please enter a password.<br/>";
+  message.className = "error-feedback";
+  document.getElementById("password-wrapper").className = "form-group has-error has-feedback";
   //message.className += " form-group has-warning has-feedback";
   //document.getElementById("message-glyph").className = "glyphicon glyphicon-remove form-control-feedback";
   //document.getElementById("password-glyph").className = "glyphicon glyphicon-remove form-control-feedback";
